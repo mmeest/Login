c76c794ca9d984f92405224ec1e5fdce0aab98b3
mmeest <martin.maasik.001@mail.ee>
2021-03-20 15:46:10 +0200

    Add project files.

---
+++ Login/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login.Models;
+
+namespace Login.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Membership model)
+        {
+            return View();
+        }
+        // GET: Account
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using(var context = new OfficeEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("login");
+        }
+    }
+}
